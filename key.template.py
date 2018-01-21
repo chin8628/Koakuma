d652be1441a098e03fbac2d6e42615bf19ba2fc7
chin8628 <boon.8628@gmail.com>
2018-01-22 01:26:27 +0700

    Added key.template

---
+++ key.template.py
@@ -0,0 +1,9 @@
+# Twittter App's token
+TOKEN = 'token'
+TOKEN_SECRET = 'secret_token'
+CONSUMER_KEY = 'consumer_key'
+CONSUMER_SECRET = 'consumer_secret'
+
+# MyAnimeList authentication
+USERNAME = ""
+PASSWORD = ""
